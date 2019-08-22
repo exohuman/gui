e96d8a1cf090b5e82f25496dd89fdc2baae238e2
Jerome Stepp <exohuman@gmail.com>
2019-08-22 15:06:10 -0400

    custom vulkan wrapper started

---
+++ rusted_vulkans_sys/Readme.md
@@ -0,0 +1,3 @@
+`sudo apt install vulkan and or related -dev`
+
+`sudo apt install llvm llvm-dev`
---
+++ rusted_vulkans_sys/wrapper.h
@@ -0,0 +1,19 @@
+// for modern linux... opther platforms will use a different define
+#define VK_USE_PLATFORM_XCB_KHR
+
+// #define VK_USE_PLATFORM_ANDROID_KHR
+// #define VK_USE_PLATFORM_FUCHSIA
+// #define VK_USE_PLATFORM_IOS_MVK
+// #define VK_USE_PLATFORM_MACOS_MVK
+// #define VK_USE_PLATFORM_METAL_EXT
+// #define VK_USE_PLATFORM_VI_NN
+// #define VK_USE_PLATFORM_WAYLAND_KHR
+// #define VK_USE_PLATFORM_WIN32_KHR
+// #define VK_USE_PLATFORM_XCB_KHR
+// #define VK_USE_PLATFORM_XLIB_KHR
+// #define VK_USE_PLATFORM_XLIB_XRANDR_EXT
+// #define VK_USE_PLATFORM_GGP
+
+
+#include <vulkan/vulkan.h>
+
