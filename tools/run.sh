f0a82577b7ef3c4b9056d7806624751377fb5c93
Jerome Stepp <exohuman@gmail.com>
2020-03-11 20:02:32 -0400

    made the run script not add unnecessary variables to linux clients

--- tools/run.sh
+++ tools/run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-export VULKAN_PATH="C:\VulkanSDK\1.1.114.0"
-export VK_SDK_PATH="C:\VulkanSDK\1.1.114.0"
-export LLVM_PATH="C:\Program Files\LLVM"
-export LIBCLANG_PATH="C:\Program Files\LLVM\bin\libclang.dll"
-export LLVM_CONFIG_PATH="C:\Program Files\LLVM\bin\llvm-config"
-export LIBCLANG_STATIC_PATH="C:\Program Files\LLVM\lib"
-export CLANG_PATH="C:\Program Files\LLVM\bin\clang.exe"
-export RUST_BACKTRACE=1 
+platform=$(uname)
+if [ platform -eq "Linux" ]
+then
+    export RUST_BACKTRACE=1 
+else
+    export VULKAN_PATH="C:\VulkanSDK\1.1.114.0"
+    export VK_SDK_PATH="C:\VulkanSDK\1.1.114.0"
+    export LLVM_PATH="C:\Program Files\LLVM"
+    export LIBCLANG_PATH="C:\Program Files\LLVM\bin\libclang.dll"
+    export LLVM_CONFIG_PATH="C:\Program Files\LLVM\bin\llvm-config"
+    export LIBCLANG_STATIC_PATH="C:\Program Files\LLVM\lib"
+    export CLANG_PATH="C:\Program Files\LLVM\bin\clang.exe"
+    export RUST_BACKTRACE=1 
+fi
+
 cd gui_examples 
 cargo run --example basic_window
 cd ..
